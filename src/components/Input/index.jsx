8248172f1a67ca3dad456d2a9fc9afc1f690bcb6
zahidalidev <m.zahidalidev@gmail.com>
2022-09-15 03:38:13 +0500

    billing price

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,13 @@
 import 'components/Input/styles.scss'
 
-const Input = ({ title, placeholder }) => (
+const Input = ({
+  title, placeholder, rightTitle, type = 'text',
+}) => (
   <div className='input-container'>
     <p className='heading'>{title}</p>
     <div className='input-wrapepr'>
-      <input className='input' placeholder={placeholder} />
+      <input className='input' type={type} placeholder={placeholder} />
+      {rightTitle && <p className='heading'>{rightTitle}</p>}
     </div>
   </div>
 )
--- src/containers/addProduct/index.jsx
+++ src/containers/addProduct/index.jsx
@@ -9,18 +9,61 @@ import 'containers/addProduct/styles.scss'
 const AddProduct = () => {
   const navigate = useNavigate()
   const [image, setImage] = useState()
+  const [billingType, setBillingType] = useState('recurring')
 
   return (
     <div className='main-product-container'>
       <div className='product-card'>
         <h2>Create a Product or Service</h2>
         <h3 className='sub-heading'>General Info</h3>
-        <div className='product-info'>
+        <div className='product-row'>
           <div className='product-field name-field'>
             <Input title='Product Name' placeholder='E.g. Website Maintainance, SEO, etc.' />
           </div>
           <div className='product-field'>
             <UploadImage image={image} setImage={setImage} title='Product Name' />
+          </div>
+        </div>
+        <hr className='horizontal-seperator' />
+        <h3 className='sub-heading'>Pricing Plans</h3>
+        <p className='sub-description'>
+          Create pricing plans for this product/service. Note that every product/service can have
+          multiple plans.
+        </p>
+        <div className='product-row'>
+          <div className='product-field name-field'>
+            <Input title='Product Name' placeholder='E.g. Website Maintainance, SEO, etc.' />
+          </div>
+          <div className='product-field'>
+            <p className='heading'>Billing Type</p>
+            <div className='billing-toggle'>
+              <Button
+                onSubmit={() => setBillingType('recurring')}
+                name='Recurring'
+                borderRadius='0px'
+                border={`0.5px solid ${colors.lightGrey2}`}
+                backgroundColor={billingType === 'recurring' ? colors.primaryLight : colors.white}
+                color={billingType === 'recurring' ? colors.primary : colors.black}
+                height='3rem'
+              />
+              <Button
+                height='3rem'
+                onSubmit={() => setBillingType('one')}
+                name='One time'
+                borderRadius='0px'
+                border={`0.5px solid ${colors.lightGrey2}`}
+                backgroundColor={billingType === 'one' ? colors.primaryLight : colors.white}
+                color={billingType === 'one' ? colors.primary : colors.black}
+              />
+            </div>
+          </div>
+        </div>
+        <div className='product-row bill-details'>
+          <div className='product-field name-field'>
+            <Input title='Price' placeholder='0.00' rightTitle='USD' type='number' />
+          </div>
+          <div className='product-field'>
+            <Input title='Bill Every' placeholder='1' type='number' />
           </div>
         </div>
         <div className='product-actions product-end'>
