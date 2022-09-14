5f5fc948340869a0e9f2d5bd7a78e7ef307ca746
zahidalidev <m.zahidalidev@gmail.com>
2022-09-14 05:26:44 +0500

    responsive survey

--- src/containers/cancellationSurvey/index.jsx
+++ src/containers/cancellationSurvey/index.jsx
@@ -1,4 +1,7 @@
 import { BiChevronLeft } from 'react-icons/bi'
+// import { IoIosAlert } from 'react-icons/io'
+
+import answers from 'utils/constants/cancellationSurvey'
 
 import 'containers/cancellationSurvey/styles.scss'
 
@@ -13,7 +16,37 @@ const CancellationSurvey = () => {
           </div>
           <p className='title'>Cancellation Survey</p>
         </div>
-        <div className='cancel-modal'>asd</div>
+        <div className='cancel-survey'>
+          <h3>We&apos;re are sad to see you go...</h3>
+          <p className='questions'>Can you tell us why you want to cancel your membership?</p>
+          <form onChange={(e) => console.log(e.target.value)}>
+            {answers.map((answer) => (
+              <div key={answer.id} className='answer-wrapper'>
+                <input
+                  type='radio'
+                  id={answer.title}
+                  className='radio-btn'
+                  name='fav_language'
+                  value={answer.title}
+                  size='10px'
+                />
+                <label className='label' htmlFor={answer.title}>
+                  {answer.title}
+                </label>
+              </div>
+            ))}
+          </form>
+          {/* <div className='alert-box'>
+            <div className='content'>
+              <IoIosAlert className='icon' />
+              <p className='description'>
+                Are you absolutely sure you want to cancel? If you do you&apos;ll lose access to so
+                many cool things. And you&apos;ll lose the chance to stay on this plan at the same
+                cost ongoing (regardless of future price increases)
+              </p>
+            </div>
+          </div> */}
+        </div>
       </div>
     </div>
   )
---
+++ src/utils/constants/cancellationSurvey.js
@@ -0,0 +1,32 @@
+const answers = [
+  {
+    id: 0,
+    title: "I did't realize it was arecurring membership",
+  },
+  {
+    id: 2,
+    title: 'Found a better solution',
+  },
+  {
+    id: 3,
+    title: "it's too expensive",
+  },
+  {
+    id: 4,
+    title: 'Bugs, things not working properly',
+  },
+  {
+    id: 5,
+    title: 'I just want to pay for a single plugin',
+  },
+  {
+    id: 6,
+    title: 'Not using WordPress anymore',
+  },
+  {
+    id: 7,
+    title: 'Other',
+  },
+]
+
+export default answers
